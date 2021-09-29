64a4e4935d9eeed22ce77173a1aefe3537e414ed
Roisaalfarizi <91373525+Roisaalfarizi@users.noreply.github.com>
2021-09-29 10:29:47 -1200

    profile.java

---
+++ jurnal 03/profile.java
@@ -0,0 +1,10 @@
+public class profile {
+	
+	public static void main(String[] args) {
+		System.out.println("Nama : Roisa");
+		System.out.println("NIM  : 6706210125");
+		System.out.println("Kelas: D3IF4501");
+		
+	}
+}
+
